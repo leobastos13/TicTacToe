ea945105c96891e9b875426b804c50fa9e99ccc9
leobastos13 <126288005+leobastos13@users.noreply.github.com>
2024-04-13 01:31:26 +0100

    tictactoe prep

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Numero from './Numero';
-import ListComent from './ListComent';
 import TicTacToe from './TicTacToe';
-import ConsumoCombustivel from './ConsumoCombustivel';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Numero></Numero>
-        <ListComent></ListComent>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
-      <ConsumoCombustivel></ConsumoCombustivel>
       <TicTacToe></TicTacToe>
-      
-      
     </div>
   );
 }
